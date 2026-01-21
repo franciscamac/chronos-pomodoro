b6f68d8f4b517703f79646d4e7532d9e5ce5bf62
franciscamac <franpms89@gmail.com>
2026-01-21 12:43:54 -0300

    Validando o formulario e criando uma nova tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,17 +1,60 @@
+import { use, useRef, useState } from 'react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { PlayCircleIcon } from 'lucide-react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreatNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+    if (!taskName) {
+      alert('Por favor, insira o nome da tarefa.');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completedDate: null,
+      interruptedDate: null,
+      durationInMinutes: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.durationInMinutes * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, //conferir
+        secondsRemaining, //conferir
+        formattedSecondsRemaining: '00:00', //conferir
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className='form' action=''>
+    <form onSubmit={handleCreatNewTask} className='form' action=''>
       <div className='formRow'>
         <DefaultInput
           labelText='Task'
           id='meuInput'
           type='text'
           placeholder='Digite a tarefa'
+          ref={taskNameInput}
         />
       </div>
 
